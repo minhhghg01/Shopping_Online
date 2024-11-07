acf02d0761e05e9098653df80ae475827fbee180
minhhghg01 <20020060@vnu.edu.vn>
2024-11-07 10:35:48 +0700

    add Theme

--- obj/Debug/net8.0/Shopping_Online.AssemblyInfo.cs
+++ obj/Debug/net8.0/Shopping_Online.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shopping_Online")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5bd09c25d290e9d30e1dfca9b860ad65ec20250")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+181196621d3bb75585fe0b4fadb2f777c787ee89")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shopping_Online")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shopping_Online")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
