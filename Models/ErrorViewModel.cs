b5bd09c25d290e9d30e1dfca9b860ad65ec20250
minhhghg01 <20020060@vnu.edu.vn>
2024-11-07 10:12:46 +0700

    khoi tao du an

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Shopping_Online.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
