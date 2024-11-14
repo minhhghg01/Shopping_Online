7b8d119af186ea369cc9f880d3df489b093daff0
minhhghg01 <20020060@vnu.edu.vn>
2024-11-14 14:25:06 +0700

    v94

--- obj/Debug/net8.0/Shopping_Online.AssemblyInfo.cs
+++ obj/Debug/net8.0/Shopping_Online.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shopping_Online")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35812e69bae93bfc0af8a130a903ff7fa542161c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98e830a8c19addc2da070e8e2364c026d45ec762")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shopping_Online")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shopping_Online")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
