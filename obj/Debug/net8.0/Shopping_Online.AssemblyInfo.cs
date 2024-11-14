5da563ca82c3d046890569f4f39bcf63abc55365
minhhghg01 <20020060@vnu.edu.vn>
2024-11-14 09:26:47 +0700

    admin export datatable js v72

--- obj/Debug/net8.0/Shopping_Online.AssemblyInfo.cs
+++ obj/Debug/net8.0/Shopping_Online.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shopping_Online")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cec43977f71750be60ce8d7756a0160e2012fc97")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c880a9cb7af43b26250c006e0bd84fdd200bb49")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shopping_Online")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shopping_Online")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
